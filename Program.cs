0c8d7988758412b2b08bbe4db92672728fe88584
Lee Harding <lee.harding@circuitpeople.com>
2020-11-13 10:37:12 -0800

    Example for NYC Taxi data in Parquet format

--- Program.cs
+++ Program.cs
@@ -2,24 +2,22 @@
 using System.IO;
 using System.Threading.Tasks;
 using Amazon.S3;
-using DiscUtils.Iso9660;
+using Parquet;
+using Parquet.Data;
 
 namespace Seekable_S3_Stream
 {
     class Program
     {
-        const string BUCKET = "rds.nsrl.nist.gov";
-        const string KEY = "RDS/current/RDS_ios.iso"; // "RDS/current/RDS_modern.iso";
-        const string FILENAME = "READ_ME.TXT";
+        const string BUCKET = "ursa-labs-taxi-data";
+        const string KEY = "2019/06/data.parquet";
         static async Task Main(string[] args)
         {
             var s3 = new AmazonS3Client();
 
             using var stream = new Cppl.Utilities.AWS.SeekableS3Stream(s3, BUCKET, KEY, 1 * 1024 * 1024, 4);
-            using var iso = new CDReader(stream, true);
-            using var file = iso.OpenFile(FILENAME, FileMode.Open, FileAccess.Read);
-            using var reader = new StreamReader(file);
-            var content = await reader.ReadToEndAsync();
+            using var parquet = new ParquetReader(stream);
+            var fields = parquet.Schema.GetDataFields();
 
             await Console.Out.WriteLineAsync($"{stream.TotalRead / (float)stream.Length * 100}% read, {stream.TotalLoaded / (float)stream.Length * 100}% loaded");
         }
