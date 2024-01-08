95b90b1f3dbed2466045e59473118f27d6a729d7
Lee Harding <lee.harding@circuitpeople.com>
2024-01-08 03:58:51 -0800

    Package updates.

--- Examples/LoadSchemaFromParquet/Program.cs
+++ Examples/LoadSchemaFromParquet/Program.cs
@@ -16,7 +16,7 @@ namespace SeekableS3Stream.Examples.LoadSchemaFromParquet
             var s3 = new AmazonS3Client();
 
             using var stream = new Cppl.Utilities.AWS.SeekableS3Stream(s3, BUCKET, KEY, 1 * 1024 * 1024, 4);
-            using var parquet = new ParquetReader(stream);
+            using var parquet = await ParquetReader.CreateAsync(stream);
             var fields = parquet.Schema.GetDataFields();
 
             await Console.Out.WriteLineAsync($"{stream.TotalRead:0,000} read {stream.TotalLoaded:0,000} loaded of {stream.Length:0,000} bytes");
